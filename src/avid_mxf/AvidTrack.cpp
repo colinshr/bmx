2d41420087eb6cb1e6bafca3ccd86deb4a340479
colin <colin>
2024-08-19 14:56:53 +0300

    updated

--- src/avid_mxf/AvidTrack.cpp
+++ src/avid_mxf/AvidTrack.cpp
@@ -659,7 +659,6 @@ void AvidTrack::CreateHeaderMetadata()
         descriptor->setContainerDuration(track_growing_duration);
     descriptor->setLinkedTrackID(AV_TRACK_ID);
     // replace essence container label with generic AAF-KLV (aka MXF) label
-    //descriptor->setEssenceContainer(MXF_EC_L(AvidAAFKLVEssenceContainer));
 
     // Preface - ContentStorage - EssenceContainerData
     EssenceContainerData *ess_container_data = new EssenceContainerData(mHeaderMetadata);
